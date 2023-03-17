cfa42df8ca4402e1a2f5e4f56f46b7c835923478
Patrício Rios <patricio.osterno1@gmail.com>
2023-03-17 15:53:43 -0300

    Adicionar arquivos de projeto.

---
+++ CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSharp.Fundamentos;
+
+namespace CSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
