0d273c6b5b313b31ac05fcb74303be03aaecfeb7
Patrício Rios <patricio.osterno1@gmail.com>
2023-03-17 22:08:08 -0300

    Aulas sobre fundamentos + exercício de lógica

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -8,6 +8,16 @@ namespace CSharp {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentários - Fundamentos", Comentários.Executar},
+                {"Variáveis e Constantes - Fundamentos", VariáveisEConstantes.Executar},
+                {"Inferência - Fundamentos", Inferencia.Executar},
+                {"Interpolação - Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados - Fundamentos", LendoDados.Executar},
+                {"Formatando Números - Fundamentos", FormatandoNumero.Executar},
+                {"Conversões - Fundamentos", Conversoes.Executar},
+                {"Teste", Teste.Executar }
+
             });
 
             central.SelecionarEExecutar();
